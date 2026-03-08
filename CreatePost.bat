d4c99a6270e45bf0c14fd7e3c817339f8e96e34e
Zhiqiang DING <zqding@hotmail.com>
2026-03-08 11:11:09 +0800

    Add script to create post

---
+++ CreatePost.bat
@@ -0,0 +1,7 @@
+set postname=%1
+
+hugo new post/%postname%.md 
+REM "C:\Program Files\Vim\vim81\gvim.exe" content/post/%postname%.md
+
+
+"D:\Program Files\Microsoft VS Code\Code.exe" content/post/%postname%.md
