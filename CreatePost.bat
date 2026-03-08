9abe1b5191f55f61e524c035b1a70ab18d1dc0bf
Zhiqiang DING <zqding@hotmail.com>
2026-03-08 11:33:33 +0800

    Add hugo

--- CreatePost.bat
+++ CreatePost.bat
@@ -4,4 +4,4 @@ hugo new post/%postname%.md
 REM "C:\Program Files\Vim\vim81\gvim.exe" content/post/%postname%.md
 
 
-"D:\Program Files\Microsoft VS Code\Code.exe" content/post/%postname%.md
+"C:\Users\dingsu\AppData\Local\Programs\Microsoft VS Code\Code.exe" content/post/%postname%.md
